9e8262f514fb648f4acf26811742d2128d83edc0
myUsername <myemail@gmail.com>
2023-11-10 03:19:25 -0800

    python exceptions

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,19 +1,18 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    quotients = []
-    for i in range(0, list_length):
+    n_list = []
+    for i in range(list_length):
         try:
-            quotient = my_list_1[i] / my_list_2[i]
+            div = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
-            quotient = 0
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            quotient = 0
+            div = 0
         except IndexError:
             print("out of range")
-            quotient = 0
-        except Exception as e:
-            pass
+            div = 0
         finally:
-            quotients.append(quotient)
+            n_list.append(div)
+    return n_list
