541c55743d6dbaa19de94088c490f5c70471c151
myUsername <myemail@gmail.com>
2023-11-30 04:10:38 -0800

    comedy task 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,8 @@
 --  lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT comedy
-FROM tv_genres
-GROUP BY name = Comedy
-DISPLAY tv_shows.title
-ORDER tv_shows.title IN ASC;
+FROM tv_shows
+GROUP BY title
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title ASC;
