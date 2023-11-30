edfa0a9b496e0265c2bf623e5f6ba1d45e1776f5
myUsername <myemail@gmail.com>
2023-11-30 03:23:25 -0800

    Number of shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+--  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- each record diplsys tv show genres
+SELECT <TV Show genre> - <Number of shows linked to this genre>
+NAME FIRST COLUMMN genre
+NAME SECOND COLUMN number_of_shows
+ORDER BY DESC 
