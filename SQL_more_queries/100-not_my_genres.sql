c2d66b2244167283eb2405538d669a0904f2ff0f
myUsername <myemail@gmail.com>
2023-11-30 05:44:00 -0800

    comedy

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -2,8 +2,8 @@
 SELECT name
 FROM tv_genres
 WHEREname NOT IN 
-SELECT name
-FROM tv_genres
+ SELECT name
+ FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter')
