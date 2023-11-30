cba01cbd47663d42d54c8a1e882dd03446a3e062
myUsername <myemail@gmail.com>
2023-11-30 04:49:52 -0800

    privileges in mysql queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2;
 -- privileges for user 1
-SHOW GRANTS ON *.* TO 'user_0d_1'@'localhost';
+SHOW GRANTS ON *.* FOR 'user_0d_1'@'localhost';
 -- privileges for user 2
-SHOW GRANTS ON *.* TO 'user_0d_2'@'localhost';
+SHOW GRANTS ON *.* FOR 'user_0d_2'@'localhost';
