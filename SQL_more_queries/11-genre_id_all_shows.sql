f195b8ff1840a2352ea7b1fb8485332a4a015808
myUsername <myemail@gmail.com>
2023-11-30 03:07:54 -0800

    show genres

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,5 @@
 -- Import the database dump of hbtn_0d_tvshows
 -- Each record displays 
-SELECT tv_shows.title, tv_show_genres.genre_id SORTED IN ASC; 
-IF NO GENRE DISPLAY NULL
+SELECT tv_shows.title, tv_show_genres.genre_id 
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
