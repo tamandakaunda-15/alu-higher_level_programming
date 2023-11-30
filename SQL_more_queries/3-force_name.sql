b0f5fac6e48dc19ae6b714358a5964d10e8a24e1
myUsername <myemail@gmail.com>
2023-11-29 23:23:31 -0800

    force name task 2

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
-a script that creates the table force_name
+-- creates the table force_name
+-- creates a table in a database
 CREATE TABLE IF NOT EXISTS $1.force_name (
     id INT,
     name VARCHAR(256) NOT NULL
