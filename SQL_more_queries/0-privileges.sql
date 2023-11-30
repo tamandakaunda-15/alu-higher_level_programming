cd3723e38cd90fb450e58489b9b6109efc53cfa0
myUsername <myemail@gmail.com>
2023-11-30 04:55:13 -0800

    task 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,7 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2;
 -- privileges for user 1
-SHOW GRANTS ON *.* FOR 'user_0d_1'@'localhost';
+SELECT grantee, privilege_type FROM information_schema.user_privileges
+WHERE grantee IN ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'");
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- privileges for user 2
-SHOW GRANTS ON *.* FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
