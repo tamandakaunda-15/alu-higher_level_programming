fbb0ff07839037070196935470a252e23a594722
myUsername <myemail@gmail.com>
2023-11-30 00:14:06 -0800

    database

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+--  lists all cities contained in the database hbtn_0d_usa
+-- record displays cities id and name and states name
+DISPLAY cities.id-cities.name-states.name
+-- sorting results in ascending order
+SORTED BY id in ASC;
