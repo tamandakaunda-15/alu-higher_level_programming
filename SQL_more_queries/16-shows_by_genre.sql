19ad00fbb9cced21c4f88a0c37f256712cbbfc89
myUsername <myemail@gmail.com>
2023-11-30 04:21:19 -0800

    taks 16 more sql

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+--  lists all Comedy shows in the database hbtn_0d_tvshows
+-- lists names containing one record in the column
+SELECT title
+FROM tv_genres
+LEFT JOIN  tv_show_genres ON tv_shows.id  = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
