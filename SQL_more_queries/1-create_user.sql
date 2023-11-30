57bc3fea8bd8991b93251e1c54ff712c5f76d765
myUsername <myemail@gmail.com>
2023-11-30 05:12:47 -0800

    all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates the MySQL server user user_0d_1 and grant all priviledges
 -- create user
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- grant privileges
-GRANT ALL PRIVILEGES FOR  * . * TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON  * . * TO 'user_0d_1@localhost' GRANT OPTION; 
