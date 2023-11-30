e8cb248a92f35d1bf1e081bbfb15d33688c10145
myUsername <myemail@gmail.com>
2023-11-30 05:40:34 -0800

    tv shows genres

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,9 @@
 -- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 SELECT name
 FROM tv_genres
-WHERE tv_genres.id NOT IN ( 
-	SELECT tv_genre_id
-	FROM  tv_shows
-	WHERE title = 'Dexter'
-);
+WHEREname NOT IN 
+SELECT name
+FROM tv_genres
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
