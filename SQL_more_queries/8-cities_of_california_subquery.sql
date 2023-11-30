b3734436d4c0ddb808a30b2454bd75f2e3cab7ae
myUsername <myemail@gmail.com>
2023-11-30 00:07:14 -0800

    more queries

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+-- states table contains one record 
+table CONTAINS ONE RECORD name= California
+-- list all rows in ascending order
+SORTED IN ORDER BY id ASC;
