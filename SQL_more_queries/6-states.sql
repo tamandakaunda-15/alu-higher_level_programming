282f6c0831c1d3da6018d82e91a6500c01427e6b
myUsername <myemail@gmail.com>
2023-11-29 23:43:22 -0800

    task 6 more queries

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) 
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) 
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
 USE hbtn_0d_usa;
+-- creates a table
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
