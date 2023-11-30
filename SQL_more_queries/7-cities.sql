333f978e310ed18b06ef8956de0b3142e38c4331
myUsername <myemail@gmail.com>
2023-11-29 23:46:59 -0800

    database creation task 6

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- create the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+-- database creation
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- using  a database
+USE hbtn_0d_usa;
+-- creating a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
