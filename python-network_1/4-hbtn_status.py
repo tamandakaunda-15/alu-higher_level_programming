90fdf1c2b704b96726167c6c7f10c68ad7270fe9
tamandakaunda-15 <tamandakaunda900@gmail.com>
2023-12-02 22:22:50 +0000

    alu python networks

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Fetches  https://alu-intranet.hbtn.io/status ."""
+"""Fetches https://alu-intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-try:
     r = requests.get("https://alu-intranet.hbtn.io/status")
-    r.raise_for_status()
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
- Except requests.exceptions.RequestException as e:
-    print(f”Request error: {e}”)
