c2b982861cc48f0bbbb76bca188a808be35c6e6f
tamandakaunda-15 <tamandakaunda900@gmail.com>
2023-12-02 19:54:50 +0000

    python networks

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes a URL and displays all HTTP methods the server will accept.
-echo "The allowed methods for $1 are $(curl -sI "$1" | awk '/Allow/ {print $2}')"
+$(curl -sI "$1" | awk '/Allow/ {print $2}')"
