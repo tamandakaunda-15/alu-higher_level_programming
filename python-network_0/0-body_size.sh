1e056be1f7f855e52f8e01f96df1f20f09488ac8
tamandakaunda-15 <tamandakaunda900@gmail.com>
2023-12-02 19:01:05 +0000

    network 0 python

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-[ $# -eq 0 ] && { echo "Please provide a URL as the argument."; exit 1; }
-
-URL="$1"
-SIZE=$(curl -sI "$URL" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r\n')
-
-echo "Size of the response body: 10 bytes"
+#sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
