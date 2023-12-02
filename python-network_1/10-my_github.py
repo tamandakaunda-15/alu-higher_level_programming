337f21778f6ccb02ea58e3c86a0d560bc8dd01f3
tamandakaunda-15 <tamandakaunda900@gmail.com>
2023-12-02 21:56:13 +0000

    python networks tasks tamandakaunda-15

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -5,8 +5,18 @@ import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
+def get_github_user_id():
+    username = "tamandakaunda-15"
+    access_token = "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"
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, access_token))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data['id']
+        print(f'Your GitHub user ID is: {user_id}')
+    else:
+        print(f'Failed to fetch user ID. Status code: {response.status_code}')
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    get_github_user_id()
