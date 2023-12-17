20f46ce9c108210091ba19d66522b30675ec530f
myUsername <myemail@gmail.com>
2023-12-17 03:43:18 -0800

    networks 1 python

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""Uses the GitHub API to display a GitHub ID based on given credentials.
+"""A script that:
+- takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id
 """
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
-def get_github_user_id():
-    username = "tamandakaunda-15"
-    access_token = "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"
-    url = 'https://api.github.com/user'
-    response = requests.get(url, auth=(username, access_token))
-
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data['id']
-        print(f'Your GitHub user ID is: {user_id}')
-    else:
-        print(f'Failed to fetch user ID. Status code: {response.status_code}')
 
 if __name__ == "__main__":
-    get_github_user_id()
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
