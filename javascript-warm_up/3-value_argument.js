f945c12d73c1d11e15a37fb45dc38c574b36f899
tamandakaunda-kaunda <tamandakaunda900@gmail.com>
2024-02-04 15:44:57 +0000

    warm up

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('process');
-let i = 0;
-let elementCount = 0;
-while (argv[i]) {
-  elementCount++;
-  i++;
-}
-if (elementCount <= 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(argv[2]);
-} 
+  console.log(process.argv[2]);
+}
