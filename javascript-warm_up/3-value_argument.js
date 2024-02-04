a150283920ef159bef2b58ac293bf22c0a5b89a8
tamandakaunda-kaunda <tamandakaunda900@gmail.com>
2024-02-04 13:52:57 +0000

    alu javascript

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,9 +3,9 @@
 function printArgs(arg1, arg2) {
   console.log(arg1 + 'is' + arg2);
 }
-if (args.length < 2) {
+if (args < 2) {
   console.log("Missing arguments");
-} else if (args.length > 2) {
+} else if (args > 2) {
   console.log("Too many arguments");
 // calling the arguments with sample arguments
 printArgs('HBTN' + 'is' + 'HBTN cool');
