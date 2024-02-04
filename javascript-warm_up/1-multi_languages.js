465cb38e88b0fbab4adf187d4a91926fa733202d
tamandakaunda-kaunda <tamandakaunda900@gmail.com>
2024-02-04 09:56:15 +0000

    javascript multilanguages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// A script that prints 3 lines, namely; C is fun, Python is cool, JavaScript is amazing;
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
