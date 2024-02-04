1fb61977b6d8633e98411a73e28f9a6c334a9daa
tamandakaunda-kaunda <tamandakaunda900@gmail.com>
2024-02-04 13:37:53 +0000

    amazing language

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//  a script that prints the first argument passed to it:
+function printArgs(arg1, arg2) {
+  console.log(arg1 + 'is' + arg2);
+}
+// calling the arguments with sample arguments
+printArgs('Javascript', 'amazing');
