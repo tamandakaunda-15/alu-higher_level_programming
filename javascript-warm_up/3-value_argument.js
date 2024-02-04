9c109ec9d016c0d6d943d28f51e3f4a50a3ac1cb
tamandakaunda-kaunda <tamandakaunda900@gmail.com>
2024-02-04 13:46:27 +0000

    arguments again

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,4 +4,4 @@ function printArgs(arg1, arg2) {
   console.log(arg1 + 'is' + arg2);
 }
 // calling the arguments with sample arguments
-printArgs('Javascript', 'amazing');
+printArgs('HBTN', 'HBTN cool');
