ce496e1947438535da9c7aeebeaa6e776a8c6d0e
tamandakaunda-kaunda <tamandakaunda900@gmail.com>
2024-02-04 15:19:42 +0000

    programming

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,5 +4,4 @@ const intNumber = parseInt(argv[2]);
 if (isNaN(intNumber)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${intNumber}`);
-} 
+  console.log(`My number: ${intNumber}`); 
