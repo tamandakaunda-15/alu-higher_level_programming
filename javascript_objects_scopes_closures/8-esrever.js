58f3f12ba341d5f146fccd2b98d0abe89eb13d33
tamandakaunda-kaunda <tamandakaunda900@gmail.com>
2024-02-04 16:51:10 +0000

    scopes, objects

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+ 
+rts.esrever = function (list) {
+  let len = list.length - 1;
+  let i = 0;
+  while ((len - i) > 0) {
+    const aux = list[len];
+    list[len] = list[i];
+    list[i] = aux;
+    i++;
+    len--;
+  }
+  return list;
+};
